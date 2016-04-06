bf941d33ccba5bd549186ac310cf0b2457111711
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2016-04-06 22:56:08 +0200

    PEP8

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,17 +36,18 @@ sys.path.insert(0,os.path.abspath('sphinxext'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [ 'sphinx.ext.autodoc',
-               'sphinx.ext.doctest',
-               'sphinx.ext.intersphinx',
-               'sphinx.ext.todo',
-               'sphinx.ext.coverage',
-               'sphinx.ext.pngmath',
-               'sphinx.ext.ifconfig',
-               'sphinx.ext.autosummary',
-               'sphinx.ext.viewcode',
-               'numpy_ext.numpydoc'
-             ]
+extensions = [
+     'sphinx.ext.autodoc',
+     'sphinx.ext.doctest',
+     'sphinx.ext.intersphinx',
+     'sphinx.ext.todo',
+     'sphinx.ext.coverage',
+     'sphinx.ext.pngmath',
+     'sphinx.ext.ifconfig',
+     'sphinx.ext.autosummary',
+     'sphinx.ext.viewcode',
+     'numpy_ext.numpydoc'
+]
 
 # Remove some numpy-linked warnings
 numpydoc_show_class_members = False
